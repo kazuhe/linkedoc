e0c137c9f7fb0a00ad92a61fafb2050b390b9a41
ohara-kazuhiro <ohara-kazuhiro@dmm.com>
2024-02-18 03:01:20 +0900

    request from client to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,33 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Responder, Result};
+use serde::Serialize;
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Serialize)]
+struct Hello {
+    message: String,
+}
+
+async fn hello() -> Result<impl Responder> {
+    let hello = Hello {
+        message: "Hello, world!".to_string(),
+    };
+    Ok(web::Json(hello))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/hello", web::get().to(hello)).service(
-            actix_files::Files::new("/", "../client/out")
-                .index_file("index.html")
-                .show_files_listing(),
-        )
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST"]);
+        App::new()
+            .wrap(cors)
+            .route("/hello", web::get().to(hello))
+            .service(
+                actix_files::Files::new("/", "../client/out")
+                    .index_file("index.html")
+                    .show_files_listing(),
+            )
     })
     .bind("0.0.0.0:8080")?
     .run()
