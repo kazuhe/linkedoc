c8ccb1866fd853b20e3f32b05f7e600a165b48ce
Ohara Kazuhiro <57878514+kazuhe@users.noreply.github.com>
2024-02-18 03:01:43 +0900

    Fix #3 (#4)
    * add actix-web
    
    * add actix-files
    
    * request from client to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Hello {
+    message: String,
+}
+
+async fn hello() -> Result<impl Responder> {
+    let hello = Hello {
+        message: "Hello, world!".to_string(),
+    };
+    Ok(web::Json(hello))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST"]);
+        App::new()
+            .wrap(cors)
+            .route("/hello", web::get().to(hello))
+            .service(
+                actix_files::Files::new("/", "../client/out")
+                    .index_file("index.html")
+                    .show_files_listing(),
+            )
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
