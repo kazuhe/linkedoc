1db4f05de6bf4fbcbb45fb277f9de068ce20879c
ohara-kazuhiro <ohara-kazuhiro@dmm.com>
2024-02-18 00:56:25 +0900

    add actix-web

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
