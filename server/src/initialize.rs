7ae7169de270c6b782714eec1dc8b09bdbfb6092
ohara-kazuhiro <ohara-kazuhiro@dmm.com>
2024-02-22 01:38:11 +0900

    add creation of configuration file to initialization feature

--- server/src/initialize.rs
+++ server/src/initialize.rs
@@ -2,10 +2,21 @@ use actix_web::{
     web::{self, ServiceConfig},
     HttpResponse, Responder,
 };
+use serde::{Deserialize, Serialize};
 use std::{fs, io, path::PathBuf};
+use std::{fs::File, io::Write};
 
 /**
- * "<HOME>/.linkedoc" のパスを取得する
+ * 設定ファイル
+ */
+#[derive(Serialize, Deserialize)]
+struct Config {
+    /** 最新のドキュメント ID */
+    latest_document_id: i32,
+}
+
+/**
+ * "<HOME>/linkedoc" のパスを取得する
  */
 fn get_linkedoc_dir() -> Result<PathBuf, io::Error> {
     let home_dir = match dirs::home_dir() {
@@ -21,10 +32,33 @@ fn get_linkedoc_dir() -> Result<PathBuf, io::Error> {
 }
 
 /**
+ * 設定ファイルのパスを取得する
+ */
+fn get_config_path() -> Result<PathBuf, io::Error> {
+    Ok(get_linkedoc_dir()?.join("config.json"))
+}
+
+/**
+ * 設定ファイルを作成する
+ */
+fn create_config() -> Result<(), io::Error> {
+    let config = Config {
+        latest_document_id: 0,
+    };
+    let config_path = get_config_path()?;
+    let mut file = File::create(config_path)?;
+    let config_json = serde_json::to_string_pretty(&config)?;
+    file.write_all(config_json.as_bytes())?;
+    Ok(())
+}
+
+/**
  * 初期化が完了しているかどうか
+ * 設定ファイルがあるかどうかで判断する
  */
 pub fn is_initialized() -> Result<bool, io::Error> {
-    if get_linkedoc_dir()?.exists() {
+    let config_path = get_config_path()?;
+    if config_path.exists() {
         return Ok(true);
     }
     Ok(false)
@@ -34,13 +68,20 @@ pub fn is_initialized() -> Result<bool, io::Error> {
  * 初期化を行う
  */
 pub async fn initialize() -> impl Responder {
-    let linkedoc_dir = get_linkedoc_dir().unwrap();
     match is_initialized() {
         Ok(true) => HttpResponse::Ok().json("Already initialized"),
-        Ok(false) => match fs::create_dir(linkedoc_dir) {
-            Ok(_) => HttpResponse::Ok().json("Initialized"),
-            Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
-        },
+        Ok(false) => {
+            let linkedoc_dir = get_linkedoc_dir().unwrap();
+            if !linkedoc_dir.exists() {
+                if let Err(err) = fs::create_dir(linkedoc_dir) {
+                    return HttpResponse::InternalServerError().json(err.to_string());
+                }
+            }
+            if let Err(err) = create_config() {
+                return HttpResponse::InternalServerError().json(err.to_string());
+            }
+            HttpResponse::Ok().json("Initialized")
+        }
         Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
     }
 }
