d67307ff320cb2f69e336b568d79b843f5812601
ohara-kazuhiro <ohara-kazuhiro@dmm.com>
2024-02-22 02:48:58 +0900

    implement with MVC architecture

---
+++ server/src/models/config.rs
@@ -0,0 +1,60 @@
+use serde::{Deserialize, Serialize};
+use std::{fs, io, path::PathBuf};
+use std::{fs::File, io::Write};
+
+/**
+ * "<HOME>/linkedoc" ディレクトリのパスを取得する
+ */
+fn get_linkedoc_dir() -> Result<PathBuf, io::Error> {
+    let home_dir = match dirs::home_dir() {
+        Some(path) => path,
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Home directory not found",
+            ))
+        }
+    };
+    Ok(home_dir.join("linkedoc"))
+}
+
+/**
+ * "<HOME>/linkedoc" ディレクトリを作成する
+ */
+pub fn create_linkedoc_dir() -> Result<(), io::Error> {
+    let linkedoc_dir = get_linkedoc_dir()?;
+    if !linkedoc_dir.exists() {
+        fs::create_dir(linkedoc_dir)?;
+    }
+    Ok(())
+}
+
+/**
+ * 設定ファイル
+ */
+#[derive(Serialize, Deserialize)]
+struct Config {
+    /** 最新のドキュメント ID */
+    latest_document_id: i32,
+}
+
+/**
+ * 設定ファイルのパスを取得する
+ */
+pub fn get_config_path() -> Result<PathBuf, io::Error> {
+    Ok(get_linkedoc_dir()?.join("config.json"))
+}
+
+/**
+* 設定ファイルを作成する
+*/
+pub fn create_config() -> Result<(), io::Error> {
+    let config = Config {
+        latest_document_id: 0,
+    };
+    let config_path = get_config_path()?;
+    let mut file = File::create(config_path)?;
+    let config_json = serde_json::to_string_pretty(&config)?;
+    file.write_all(config_json.as_bytes())?;
+    Ok(())
+}
